88dfeb167df54f014c1a39f2595c0de693e728d8
Allan Lewis <allanlewis99@gmail.com>
2016-05-14 00:03:29 +0100

    Autopep8

--- ssltest.py
+++ ssltest.py
@@ -23,25 +23,43 @@ def pprint_response(endpoint, **params):
 
 
 def main():
-#    pprint_response('info')
-    host_info = get_response('analyze', host='www1.firstdirect.com', fromCache='on', all='done')
+    #    pprint_response('info')
+    host_info = get_response(
+        'analyze',
+        host='www1.firstdirect.com',
+        fromCache='on',
+        all='done')
     pprint(host_info)
     ip_addrs = [endpoint['ipAddress'] for endpoint in host_info['endpoints']]
     for addr in ip_addrs:
         print('***', addr)
-        pprint_response('getEndpointData', host='www1.firstdirect.com', fromCache='on', s=addr)
+        pprint_response(
+            'getEndpointData',
+            host='www1.firstdirect.com',
+            fromCache='on',
+            s=addr)
 
 
 class SSLTest(object):
+
     def __init__(self, host, use_cache=True):
-        params = dict(fromCache='on', all='done') if use_cache else dict(startNew='on', all='done')
+        params = dict(
+            fromCache='on',
+            all='done') if use_cache else dict(
+            startNew='on',
+            all='done')
         self._host_info = get_response('analyze', host=host, **params)
         ip_addr = self._host_info['endpoints'][0]['ipAddress']
-        self._endpoint_info = get_response('getEndpointData', host=host, fromCache='on' if use_cache else '', s=ip_addr)
+        self._endpoint_info = get_response(
+            'getEndpointData',
+            host=host,
+            fromCache='on' if use_cache else '',
+            s=ip_addr)
         self.grade = self._endpoint_info['grade']
-        self.drown_vulnerable = self._endpoint_info['details']['drownVulnerable']
+        self.drown_vulnerable = self._endpoint_info[
+            'details']['drownVulnerable']
         self.poodle_vulnerable = self._endpoint_info['details']['poodle']
 
 if __name__ == '__main__':
-#    main()
+    #    main()
     pprint(vars(SSLTest('www1.firstdirect.com')))
